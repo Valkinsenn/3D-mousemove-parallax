3565b3e3ce4ad6089c55941eea7aa3bcfd813fa5
Valkinsenn <valkinsenn@gmail.com>
2021-06-09 00:38:10 -0500

    Added a custom cursor, just for kicks.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
+// Clears the console
+console.clear()
+
 // DOM Queries
-const container = document.querySelector(".card-container")
 const body = document.body
+const container = document.querySelector(".card-container")
+const cursor = document.querySelector(".cursor")
+
+// Main code
+document.addEventListener("mousemove", event => {
+  cursor.setAttribute(
+    "style",
+    `top: ${event.pageY - 10}px; left:${event.pageX - 10}px`
+  )
+})
+
+document.addEventListener("click", () => {
+  cursor.classList.add("expand")
+
+  setTimeout(() => {
+    cursor.classList.remove("expand")
+  }, 500)
+})
 
 body.addEventListener("mousemove", event => {
   let x = (window.innerWidth / 2 - event.pageX) / 30
